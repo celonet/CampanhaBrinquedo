7a2ca6a25e244dedffc6bc78121dbb5a111e4fee
Marcelo Lopes da Silva <celo-music@hotmail.com>
2018-12-05 22:16:39 -0200

    create view model and note

---
+++ src/CampanhaBrinquedo.Domain/Entities/Child/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CampanhaBrinquedo.Domain.Entities.Child
+{
+    public class Note
+    {
+        public Note(string observation)
+        {
+            Date = DateTime.Now;
+            Observation = observation;
+        }
+
+        public DateTime Date { get; private set; }
+        public string Observation { get; private set; }
+    }
+}
